2534248ca43d21eb223a726a492173b08907894a
Arren De Manuel <ademanue@ucsd.edu>
2020-01-11 10:03:25 -0800

    added new test passwords

--- src/Backend/tb/PasswordGeneratorTB.java
+++ src/Backend/tb/PasswordGeneratorTB.java
@@ -6,6 +6,12 @@ public class PasswordGeneratorTB {
         //Instantiate a new PasswordGenerator
         PasswordGenerator passGen = new PasswordGenerator();
 
-        System.out.println(passGen.genRandomPassword(8, true, false, false));
+        //Generate some passwords here
+        String passAll10 = passGen.genRandomPassword(10, true, true, true);
+        String passAll20 = passGen.genRandomPassword(20, true, true, true);
+        String passAll5 = passGen.genRandomPassword(5, true, true, true);
+        String passLetters = passGen.genRandomPassword(10, true, false, false);
+        String passNumbers = passGen.genRandomPassword(10, false, true, false);
+        String passSpecial = passGen.genRandomPassword(10, false, false, true);
     }
 }
