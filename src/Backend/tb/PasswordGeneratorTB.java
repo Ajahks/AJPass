ac7dd1beb509ddae475472714a4ea3fe32652774
Arren De Manuel <ademanue@ucsd.edu>
2020-01-16 21:06:45 -0800

    Added some print cases for the password options

--- src/Backend/tb/PasswordGeneratorTB.java
+++ src/Backend/tb/PasswordGeneratorTB.java
@@ -13,5 +13,12 @@ public class PasswordGeneratorTB {
         String passLetters = passGen.genRandomPassword(10, true, false, false);
         String passNumbers = passGen.genRandomPassword(10, false, true, false);
         String passSpecial = passGen.genRandomPassword(10, false, false, true);
+
+        System.out.println(passAll10);
+        System.out.println(passAll20);
+        System.out.println(passAll5);
+        System.out.println(passLetters);
+        System.out.println(passNumbers);
+        System.out.println(passSpecial);
     }
 }
