1539857c8400797e535b3b059d170b8347a03a05
Arren De Manuel <ademanue@ucsd.edu>
2020-01-16 21:42:37 -0800

    started password saver class

---
+++ src/Backend/PasswordSaver.java
@@ -0,0 +1,14 @@
+package Backend;
+
+//This class is in charge of holding a dictionary of passwords to urls
+//This class is also in charge of saving passwords to an encrypted file
+//Also loads the passwords from encrypted file on startup
+public class PasswordSaver {
+
+    //Adds a password to the dictionary, if the password already exists, then update it
+    private void AddPassword(String url, String password){
+
+    }
+
+    //
+}
