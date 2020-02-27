36d933ee40736d944e354b3b50da7a779d882b72
Arren De Manuel <ademanue@ucsd.edu>
2020-02-26 19:13:42 -0800

    created the data structure we will be saving account info to

--- src/Backend/LoginInfo.java
+++ src/Backend/LoginInfo.java
@@ -1,18 +1,41 @@
 package Backend;
 
 import java.util.Hashtable;
-import java.util.List;
 
 // Data structure to hold the login information for the website
 public class LoginInfo {
     Hashtable<String, String> info = new Hashtable<String,String>(); // This is where we will store the information
 
-    LoginInfo(String alias, String url, String username, String password){
+    LoginInfo(String alias, String url, String username, String password, String secretKey){
         info.put("alias", alias);
         info.put("url", url);
 
         // Encrypt the username and password
-        
+        String encryptedUsername = PasswordCipher.encrypt("username", secretKey);
+        info.put("username", encryptedUsername);
+
+        String encryptedPassword = PasswordCipher.encrypt("username", secretKey);
+        info.put("password", encryptedUsername);
+    }
+
+    // Grab the alias (will be used for display)
+    public String GetAlias(){
+        return info.get("alias");
+    }
+
+    // Grab the url
+    public String GetUrl(){
+        return info.get("url");
+    }
+
+    // Return the username given the secret key
+    public String GetUsername(String secretKey){
+        return PasswordCipher.decrypt(info.get("username"), secretKey);
+    }
+
+    // Return the password given the secretKey
+    public String GetPassword(String secretKey){
+        return PasswordCipher.decrypt(info.get("password"), secretKey);
     }
 
 }
--- src/Backend/PasswordSaver.java
+++ src/Backend/PasswordSaver.java
@@ -7,8 +7,6 @@ public class PasswordSaver {
 
     //Adds a password to the dictionary, if the password already exists, then update it
     private void AddPassword(String url, String password){
-        Encryptor encryptor = new Encryptor();
-        encryptor.encrypt()
     }
 
     //
